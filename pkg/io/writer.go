6d34f2522107ff2a8c48a7a4006d292d3effc43e
Vijayender Joshi <vijayender.joshi@gmail.com>
2020-12-17 15:15:22 +0900

    chore: update closer util function (#2)

--- pkg/io/writer.go
+++ pkg/io/writer.go
@@ -4,13 +4,46 @@ import (
 	"io"
 )
 
-func AddCloser(w io.Writer) io.WriteCloser {
+// A WriteSyncer is an io.Writer that can also flush any buffered data.
+type WriteSyncer interface {
+	io.Writer
+
+	Sync() error
+}
+
+// A WriteCloserSync is an io.Writer that implements both io.WriteCloser and Sync.
+type WriteCloserSync interface {
+	io.Writer
+
+	io.WriteCloser
+	WriteSyncer
+}
+
+func AddCloserSync(w io.Writer) WriteCloserSync {
 	switch w := w.(type) {
 	case io.WriteCloser:
-		return w
+		return writeCloser{w}
+	case WriteSyncer:
+		return writeSyncer{w}
 	default:
 		return writerWrapper{w}
 	}
+}
+
+type writeCloser struct {
+	io.WriteCloser
+}
+
+func (w writeCloser) Sync() error {
+	return w.Close()
+}
+
+type writeSyncer struct {
+	WriteSyncer
+}
+
+func (w writeSyncer) Close() error {
+	return w.Sync()
 }
 
 type writerWrapper struct {
@@ -18,5 +51,9 @@ type writerWrapper struct {
 }
 
 func (w writerWrapper) Close() error {
+	return nil
+}
+
+func (w writerWrapper) Sync() error {
 	return nil
 }
